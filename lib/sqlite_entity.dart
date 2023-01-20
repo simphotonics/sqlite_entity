4e4d8fcb11a0fec767e7a632443457ee734fc0fd
simphotonics <git@simphotonics.com>
2023-01-20 18:32:48 +0000

    Renamed library.

---
+++ lib/sqlite_entity.dart
@@ -0,0 +1,18 @@
+library sqlite_entity;
+
+export 'src/annotations/generate_model.dart';
+export 'src/annotations/generate_sqlite_init.dart';
+export 'src/entities/anchored_column.dart';
+export 'src/entities/column.dart';
+export 'src/entities/constraint.dart';
+export 'src/entities/dependency.dart';
+export 'src/entities/expr_format.dart';
+export 'src/entities/foreign_key.dart';
+export 'src/entities/foreign_key_action.dart';
+export 'src/entities/foreign_key_constraint.dart';
+export 'src/entities/index.dart';
+export 'src/entities/model.dart';
+export 'src/entities/model_definition.dart';
+export 'src/entities/sqlite_expression.dart';
+export 'src/entities/sqlite_type.dart';
+export 'src/entities/table.dart';
