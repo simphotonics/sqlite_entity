a5696343a1283167f54548557415f178846becf9
simphotonics <git@simphotonics.com>
2023-06-29 09:53:25 +0100

    Updated example.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,65 +1,69 @@
-import 'package:ansicolor/ansicolor.dart';
 import 'package:sqlite_entity/sqlite_entity.dart';
 
-class TeamDefinition extends ModelDefinition {
+class PlayerTable extends Table {
+  /// Player id.
+  static const id = Column<int>(constraints: {
+    Constraint.primaryKey,
+    Constraint.notNull,
+  });
 
-  final id = Column<int>(
-    constraints: {
-      Constraint.PRIMARY_KEY,
-    },
-      defaultValue: 0,
-  );
+  /// Player status
+  static const isActive = Column<bool>();
 
-  final pid1 = Column<int>(
+  /// First name of player.
+  static const firstName = Column<String>();
+
+  /// Last name of player.
+  static const lastName = Column<String>();
+}
+
+class TeamTable extends ModelDefinition {
+  static const id = Column<int>(
     constraints: {
-      Constraint.NOT_NULL,
+      Constraint.primaryKey,
+      Constraint.notNull,
     },
     defaultValue: 0,
   );
 
-  final test = const Column<bool>(
-    constraints: {Constraint.PRIMARY_KEY},
-    defaultValue: false,
+  static const pid1 = Column<int>(
+    constraints: {
+      Constraint.notNull,
+    },
+    defaultValue: 0,
   );
 
-  final pid2 = const Column<int>(constraints: {
-    Constraint.NOT_NULL,
-    },
-    defaultValue: 1);
+  static const pid2 = Column<int>(constraints: {
+    Constraint.notNull,
+  }, defaultValue: 1);
 
   /// Foreign Key
-  final foreignKey1 = const ForeignKey(
-      parentColumn: Player.$id,
-      childColumn: Team.$pid1,
+  static const foreignKey1 = ForeignKey(
+      parentKey: PlayerTable.id,
+      childKey: TeamTable.pid1,
       constraints: {
-        ForeignKeyConstraint.DEFERRABLE,
-        ForeignKeyConstraint.INITIALLY_DEFERRED
+        ForeignKeyConstraint.deferrable,
+        ForeignKeyConstraint.initallyDeferred,
       });
 
-  final foreignKey2 = const ForeignKey(
-    parentColumn: Player.$id,
-    childColumn: Team.$pid2,
+  static const foreignKey2 = ForeignKey(
+    parentKey: PlayerTable.id,
+    childKey: TeamTable.pid2,
   );
 
-  final differentPlayers = const CheckConstraint<Team>(expressions: [
-    NotEqual(
-      Team.$pid1,
-      Team.$pid2,
+  final differentPlayers = const CheckConstraint(
+    NotEqual<int>(
+      pid1,
+      pid2,
     ),
-    Expr(
-        left: ColumnLiteral(Team.$id),
-        right: Literal(int(187)),
-        operatorName: '!=',
-        exprFormat: ExprFormat.OPERATOR),
-  ]);
+  );
 
   static const uniqueTeams = Index(
     isUnique: true,
-    name: 'unique_teams',
-    expressions: [
-      Min(Team.$pid1, Team.$pid2),
-      Max(Team.$pid1, Team.$pid2),
-    ],
+    exprs: {
+      Min(TeamTable.pid1, TeamTable.pid2),
+      Max(TeamTable.pid1, TeamTable.pid2),
+    },
   );
 }
 
@@ -85,37 +89,6 @@ class Team extends Model {
 
   final int pid2;
 
-  /// Column: Team.id.
-  static const $id = AnchoredColumn<Team, int>(
-    constraints: {
-      Constraint.PRIMARY_KEY,
-    },
-    name: 'id',
-  );
-
-  /// Column: Team.pid1.
-  static const $pid1 = AnchoredColumn<Team, int>(
-    constraints: {
-      Constraint.NOT_NULL,
-    },
-    name: 'pid1',
-  );
-
-  /// Column: Team.test.
-  static const $test = AnchoredColumn<Team, Boolean>(
-    constraints: {
-      Constraint.PRIMARY_KEY,
-    },
-    name: 'test',
-  );
-
-  /// Column: Team.pid2.
-  static const $pid2 = AnchoredColumn<Team, int>(
-    constraints: {
-      Constraint.NOT_NULL,
-    },
-    name: 'pid2',
-  );
 
   /// Converts a [Team] to [Map<String,dynamic>].
   @override
@@ -137,127 +110,4 @@ class Team extends Model {
       'pid2': pid2,
     };
   }
-}
-
-class Player extends Model {
-  const Player(
-      {required this.id,
-      required this.isActive,
-      required this.firstName,
-      required this.timeStamp});
-
-  /// Converts a [Map<String,dynamic>] to a [Player].
-  Player.fromMap(Map<String, dynamic> map)
-      : id = map['id'],
-        isActive = map['isActive'],
-        firstName = map['firstName'],
-        timeStamp = map['timeStamp'];
-
-  final int id;
-
-  final bool isActive;
-
-  /// First name of player.
-  final String firstName;
-
-  /// Time stamp encoded as [microsecondsSinceEpoch].
-  final int timeStamp;
-
-  /// Column: Player.id.
-  static const $id = AnchoredColumn<Player, int>(
-    constraints: {
-      Constraint.PRIMARY_KEY,
-    },
-    defaultValue: int(99),
-    name: 'id',
-  );
-
-  /// Column: Player.isActive.
-  static const $isActive = AnchoredColumn<Player, Boolean>(
-    defaultValue: Boolean(true),
-    name: 'isActive',
-  );
-
-  /// Column: Player.firstName.
-  static const $firstName = AnchoredColumn<Player, Text>(
-    defaultValue: Text('Thomas'),
-    name: 'firstName',
-  );
-
-  /// Column: Player.timeStamp.
-  static const $timeStamp = AnchoredColumn<Player, int>(
-    constraints: {
-      Constraint.NOT_NULL,
-    },
-    name: 'timeStamp',
-  );
-
-  /// Converts a [Player] to [Map<String,dynamic>].
-  @override
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'isActive': isActive,
-      'firstName': firstName,
-      'timeStamp': timeStamp,
-    };
-  }
-
-  /// Creates a [Map<String, dynamic>] representing a [Player]
-  /// object without a primary key. The primary key is assigned by the
-  /// database during insertion.
-  static Map<String, dynamic> initMap(
-      {required bool isActive,
-      required String firstName,
-      required int timeStamp}) {
-    return {
-      'isActive': isActive,
-      'firstName': firstName,
-      'timeStamp': timeStamp,
-    };
-  }
-}
-
-// To run this program navigate to
-// the folder 'directed_graph/example'
-// in your terminal and type:
-//
-// # dart bin/example.dart
-//
-// followed by enter.
-void main() {
-  final blue = AnsiPen()..blue(bold: true);
-  final magenta = AnsiPen()..magenta(bold: true);
-  final green = AnsiPen()..green(bold: true);
-
-  print(magenta('Team.\$id'));
-  print(blue(Team.$id.toString()));
-  print('');
-  print(magenta('Min(Team.\$pid1, Player.\$id)'));
-  print(blue(Min(Team.$pid1, Player.$id).toString()));
-  print('');
-  print(magenta('TeamDefinition().differentPlayers'));
-  print(blue(TeamDefinition().differentPlayers.toString()));
-  print('');
-  print(magenta('TeamDefinition.uniqueTeams'));
-  print(blue(TeamDefinition.uniqueTeams.toString()));
-  print('');
-  var column = TeamDefinition().pid1;
-
-  print(magenta('Column pid1.'));
-  print(blue(column.toString()));
-
-  print('');
-  print(magenta('Overriding name of column pid1. New name: ') +
-      green('PID_1') +
-      '.');
-  column = column.overrideWith(name: 'PID_1');
-  print(blue(column.toString()));
-
-  print('');
-  print(magenta('Print string value of constraint ') +
-      blue('${Constraint.PRIMARY_KEY}: ') +
-      green(Constraint.PRIMARY_KEY.stringValue) +
-      '.');
-  print(blue(Constraint.PRIMARY_KEY.toString()));
 }
